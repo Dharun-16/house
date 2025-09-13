042d65bf864ae9141e5202f10abdfbc27a1d8dcc
Dharun Sanjai <sanjaisanjai464@gmail.com>
2025-09-13 16:08:45 +0530

    Add files via upload

---
+++ HousingRegression-hyper/HousingRegression-hyper/hyperparameter_tuning.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "41f3b0f4-760f-46db-81cd-bff6b43bc245",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
